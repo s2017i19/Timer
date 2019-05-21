e4b1c23d1dff087c27536fc88fc10dde28ae307f
Yura Choi <s2017i19@e-mirim.hs.kr>
2019-05-21 15:34:19 +0900

    #2 타이머 컨트롤과 택스트박스 컨트롤을 사용하여 카운트 기능 구현

--- Timer/Form1.cs
+++ Timer/Form1.cs
@@ -7,13 +7,66 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using Microsoft.VisualBasic;
+
 namespace Timer
 {
     public partial class Form1 : Form
     {
-        public Form1()
+		int CountOrgNum = 0;
+
+		private bool IntCheck()
+		{
+			if (Information.IsNumeric(this.txtNum.Text))
+			{
+				return true;
+			}
+			else
+			{
+				MessageBox.Show("숫자만 입력하세요!", "알림",
+					MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return false;
+			}
+		}
+
+		public Form1()
         {
             InitializeComponent();
         }
-    }
+
+		private void BtnCount_Click_1(object sender, EventArgs e)
+		{
+			if (IntCheck())
+			{
+				this.CountOrgNum = Convert.ToInt32(this.txtNum.Text);
+				this.Timer.Enabled = true;
+				this.txtNum.ReadOnly = true;
+			}
+			else
+			{
+				this.txtNum.Text = "";
+				this.txtNum.Focus();
+			}
+		}
+
+		private void Timer_Tick(object sender, EventArgs e)
+		{
+			if(CountOrgNum < 1) //아직 카운트 할 숫자가 남아있는 경우
+			{
+				this.Timer.Enabled = false;
+				this.txtNum.ReadOnly = false;
+				this.txtNum.Text = "";
+				this.txtCountDown.Text = "";
+				MessageBox.Show("펑!", "알림",
+					MessageBoxButtons.OK, MessageBoxIcon.Information);
+				this.txtNum.Focus();
+			}
+			else
+			{
+				CountOrgNum--;
+				this.txtCountDown.Text = Convert.ToString(CountOrgNum);
+			}
+
+		}
+	}
 }
